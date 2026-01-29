0d4a1f9caad5ee3541449eb3908d05254efd913e
josueantoniolara <68356721+josueantoniolara@users.noreply.github.com>
2026-01-29 00:44:20 -0600

    conceccion DB

--- Proyecto/DataBase/DB.js
+++ Proyecto/DataBase/DB.js
@@ -5,4 +5,15 @@ const conexion = mysql.createConnection({
     user:'root',
     password:'',
     database:'crud_nodejs_db'
+});
+
+conexion.connect((error)=>{
+    if(error){
+        console.log('El error de la conexión es : ' + error);
+        return
+    }
+    console.log('Conexion a la base de datos MySQL exitosa');
 })
+
+//Exportamos la conexion
+module.exports = conexion;
--- Proyecto/router.js
+++ Proyecto/router.js
@@ -1,9 +1,18 @@
 const  express = require('express');
 const router = express.Router();
+//Importamos la ruta de la DB
+const conexion = require('./DataBase/DB');
+
 
 //motor de plantillas
-router.get('/contacto', (req, res)=>{
-    res.send('CONTACTO');
+router.get('/', (req, res)=>{
+   conexion.query('SELECT ID, USER, ROL FROM users',(error, results)=>{
+    if(error){
+        throw error;
+    }else{
+        res.send(results); 
+    }
+   }) 
 })
 
 //Exportar el modulo
